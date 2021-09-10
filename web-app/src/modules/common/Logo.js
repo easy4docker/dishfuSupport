8eb8d2ac551bf5ff1075981ad353262b31483a9a
easy4docker <easy4docker@gmail.com>
2021-09-10 09:01:37 -0700

    Update Logo.js

--- web-app/src/modules/common/Logo.js
+++ web-app/src/modules/common/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Logo() {
   return (
     <div className="border rounded p-1 m-0" style={{color:'yellow', fontSize:'1.5rem'}}>
-    菜譜<span className="ml-1" style={{fontSize:'1rem'}}>Dishfu</span>
+    菜譜<span className="ml-1" style={{fontSize:'1rem'}}>SupportCenter</span>
     </div>
   )
 }
