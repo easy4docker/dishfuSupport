5e4b3f5cc42f740e39f79333933577cd163d7f9b
easy4docker <easy4docker@gmail.com>
2021-09-19 22:52:14 -0700

    Update applicationsList.js

--- web-app/src/modules/authUsersAdmin/applicationsList.js
+++ web-app/src/modules/authUsersAdmin/applicationsList.js
@@ -69,43 +69,8 @@ function ApplicationsList(props) {
   }
 */
   const inputForm = ()=> (
-   <Container className="mb-3 pb-3">
-      <Alert variant="secondary mt-3">
-        <Alert.Heading>If you have the Supie role</Alert.Heading>
-        <ul>
-          <li>You can be grocery stores, farms or restaurants.</li>
-          <li>You can make plate based on receipt and send back to Foodie for validation.</li>
-          <li> You needs to provide a real address (not PD box), can view the receipts from Foodie.</li>
-        </ul>
-      </Alert>
-
-      <Form>
-        <Form.Group>
-          <Form.Label><b>Supie Address:</b> <span className="form_notice_info">* Required</span></Form.Label>
-          <AddressInput passAddress={setAddress} defaultValue={address}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label><b>Contact Phone:</b> <span className="form_notice_info">* Required</span></Form.Label>
-          <Form.Control defaultValue={phone} placeholder="Input Contact Phone" as="textarea" 
-            rows="1"
-            onChange={onPhoneChanged} />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label><b>Your Qualification:</b> <span className="form_notice_info">* Required</span></Form.Label>
-          <Form.Control defaultValue={qualification} placeholder="Input Qualification" as="textarea" 
-            rows="1"
-            onChange={onQualificationChanged } />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label><b>Application Description:</b> <span className="form_notice_info">(Option)</span> </Form.Label>
-          <Form.Control defaultValue={description} placeholder="Input Description" as="textarea" 
-            rows="3"
-            onChange={onDescriptionChanged } />
-        </Form.Group>
-        <Button className="btn btn-info m-0 mr-3" disabled={!isSubmit()} onClick={submitApplication}>
-          <FontAwesomeIcon size="1x" icon={faCloudUploadAlt} className="m-0" /> Submit
-        </Button>
-      </Form>
+   <Container className="mb-3 p-3">
+     Application list
    </Container>);
 
    return (!success) ? inputForm() : successForm()
